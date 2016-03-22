613105852ea70e4f251b6be0cf424081a376898d
Charles Gaydon <charles.gaydon@insa-lyon.fr>
2016-03-22 19:59:16 +0100

    Mise en forme affichage.

--- Affichage_phase.R
+++ Affichage_phase.R
@@ -1,12 +1,17 @@
 rm(list = ls())
 setwd("~/Documents/ProjetCEvolutionRep")
 
-
-pdf("Phase.pdf")
-d <- read.table("PhaseNpT50pA26Pres2", header = F, col.names = c("T", "Ainit", "Etat"))
+titre = "Phase2pT25pA9Pres2"
+titreG = paste(titre,".pdf",sep = "")
+titreT = paste(titre,".txt",sep = "")
+pdf(titreG)
+d <- read.table(titreT, header = F, col.names = c("T", "Ainit", "Etat"))
 etat <- factor(d$"Etat")
 mescouleurs <- rainbow(length(levels(etat)))
-plot(d$"T", d$"Ainit", pch = 19, col = mescouleurs[etat])
+plot(d$"T", d$"Ainit", pch = 19, col = mescouleurs[etat],
+     main = "Diagramme de phase de la culture bactérienne",
+     xlab = "Durée avant changement de milieu",
+     ylab = "Concentration initiale en glucose")
 legend("topleft", inset = 0.02, pch = 19, legend = levels(etat), col = mescouleurs)
 dev.off()
 
